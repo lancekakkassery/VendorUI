049c404561290ced0f4ccd05ab66a385e616044c
kazokd <kazuya.okada1@gmail.com>
2024-11-19 19:01:47 -0600

    delete

--- server/app.py
+++ server/app.py
@@ -5,16 +5,7 @@ app = Flask(__name__)
 cors = CORS(app, origins='*')
 
 @app.route('/api/users', methods = ['GET'])
-def users():
-    return jsonify(
-        {
-            'users':[
-                'arpan',
-                'zach',
-                'jessie'
-            ]
-        }
-    )
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
