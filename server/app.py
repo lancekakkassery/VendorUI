dcb6efd7523e17979a98a551ae03abdfd2738013
kazokd <kazuya.okada1@gmail.com>
2024-11-21 15:00:08 -0600

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,27 +1,33 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,Response
 from flask_cors import CORS
 import pandas as pd
 import json
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
 cors = CORS(app, origins='*')
-
+engine = create_engine('sqlite:///vendor.db')
 
 # Load the JSON file into a pandas DataFrame
-@app.route('/') # route is the URL or HTML key, ex. file/ vs file/home
+df = pd.read_json('retail.json') 
+df.to_sql('invent_data', engine, if_exists='replace', index=False)
+@app.route('/products', methods=['GET']) 
+def get_products(): 
+    conn = engine.connect() 
+    query = 'SELECT * FROM invent_data'
+    result = conn.execute(query) 
+    products = [dict(row) for row in result] 
+    conn.close()
+    return jsonify(products)
 def read_file():
     # Open the JSON file
     with open('retail.json', 'r') as file:
-        # Load and parse the JSON data
-        list_of_dict = json.load(file)
-        output_list=[]
-        for dict in list_of_dict:
-            for key in dict:
-                val=dict[key]
-                output_list.append(f'"{key}":"{val}"')
-        # output_string=', '.join(output_list)
-    return json.dumps(output_list)
-    # return json.loads('{"key":"val"}')
+        # Load data as pandas dataframe
+        df = pd.read_json(file)
+        df['order_date_time'] = df['order_date_time'].astype(str)
+    data = df.to_dict(orient='records')
+    formatted_json = json.dumps(data, indent=2)
+    return Response(formatted_json, mimetype='application/json')
 # Access the data
 
 
